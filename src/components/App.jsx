ddd6baa5b87c35baab300c4ea955a20e7c42b453
Daniel Cunha <danielbrandaodacunha@gmail.com>
2018-07-27 14:39:25 -0300

    Auth fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import CustomerDetailedPage from '../pages/Customer/CustomerDetailedPage';
 import CustomerForm from './Customer/CustomerForm';
 import Menu from './Menu/Menu';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import withAuthentication from '../hoc/withAuthentication';
 import UserPage from '../pages/User/UserPage';
@@ -17,53 +17,59 @@ import UserForm from './User/UserForm';
 
 // library.add(faStroopwafel)
 
-const App = ({auth, doLogout}) =>
+const App = ({ auth, doLogout }) =>
   <Router className="content">
     <Row >
       <Col md={2} xs={1}>
-      <Menu auth={auth} logout={doLogout} className="col col-3" />
+        <Menu auth={auth} logout={doLogout} className="col col-3" />
       </Col>
       <Col md={10} xs={15}>
-      <Switch className="col col-9">
-        <Route exact path="/"
-          component={HomePage}
-        />
-        <Route exact path="/customer"
-          component={CustomerPage}
-        />
+        <Switch className="col col-9">
+          <Route exact path="/"
+            component={HomePage}
+          />
+          <Route exact path="/customer"
+            component={CustomerPage}
+          />
 
-        <Route exact path="/customer/:id"
-          component={CustomerDetailedPage}
-        />
+          <Route exact path="/customer/:id"
+            component={CustomerDetailedPage}
+          />
 
-        <Route exact path="/customerManage/:id"
-          component={CustomerForm}
-        />
+          <Route exact path="/customerManage/:id"
+            component={CustomerForm}
+          />
 
-        <Route exact path="/createCustomer"
-          component={CustomerForm}
-        />
+          <Route exact path="/createCustomer"
+            component={CustomerForm}
+          />
+          {auth.manager === 'S' &&
 
-        <Route exact path="/user"
-          component={UserPage}
-        />
+            <Route exact path="/user"
+              component={UserPage}
+            />
+          }
+          {auth.manager === 'S' &&
+            <Route exact path="/user/:id"
+              component={UserDetailedPage}
+            />
+          }
+          {auth.manager === 'S' &&
+            <Route exact path="/userManage/:id"
+              component={UserForm}
+            />
+          }
+          {auth.manager === 'S' &&
+            <Route exact path="/createUser"
+              component={UserForm}
+            />
+          }
 
-        <Route exact path="/user/:id"
-          component={UserDetailedPage}
-        />
+          <Route path="*"
+            component={HomePage}
+          />
 
-        <Route exact path="/userManage/:id"
-          component={UserForm}
-        />
-
-        <Route exact path="/createUser"
-          component={UserForm}
-        />
-
-        <Route path="*"
-          component={HomePage}
-        />
-      </Switch>
+        </Switch>
       </Col>
 
 
--- src/hoc/withAuthentication.jsx
+++ src/hoc/withAuthentication.jsx
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LoginPage from '../pages/Login/LoginPage';
 import { doLogout } from '../actions/auth';
+import { withRouter } from 'react-router-dom';
 
 const withAuthentication = (Component) => {
-  class WithAuthentication extends React.Component {    
-    render() {           
+  class WithAuthentication extends React.Component {
+    render() {
       return (
         <div>
-        {
-        this.props.auth.authenticated?<Component auth={this.props.auth} doLogout={this.props.doLogout} />:<LoginPage />
-      }
-      </div>
-    );
+          {
+            this.props.auth.authenticated ? <Component auth={this.props.auth} doLogout={this.props.doLogout} /> : <LoginPage />
+          }
+        </div>
+      );
     }
   }
 
-  const mapState = ( state ) => ({
+  const mapState = (state) => ({
     auth: state.auth
   });
 

a11cf8b77966807b2255bd1561b51bedac281b1d
Daniel Cunha <danielbrandaodacunha@gmail.com>
2019-07-30 16:07:53 -0300

    Services

--- src/components/Order/ServiceForm.jsx
+++ src/components/Order/ServiceForm.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Panel, FormGroup, FormControl, ControlLabel, Button, Glyphicon, Row, Col } from 'react-bootstrasp';
+
+/** 
+  Case 1: (Transportation)
+  Ask: Company, Document, type(Plane, Ship), Origin, departure, Destination, arrival, +Info
+
+  Case 2: (Other services)
+  Ask: Company, document, begins_at, ends_at, +Info
+
+**/
 
 const ServiceForm = ({addService}) => {
   return (
     <div>
-      Test
+      
     </div>
   );
 };
